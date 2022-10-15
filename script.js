c4844fe991c168042896bf3718727c212984a892
Diego Lopez <diego.lopez.19@hotmail.es>
2022-10-15 14:28:09 -0600

    the new method implement with for each

--- script.js
+++ script.js
@@ -71,7 +71,73 @@ const currencies = new Map([
   ['GBP', 'Pound sterling'],
 ]);
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
-console.log(currencies)
+
 /////////////////////////////////////////////////
+/*
+let arr = [ "a", "b", "c", "d", "e"];
+
+//arr.slice(2); // This extract 
+console.log( arr.slice(2));
+console.log( arr.slice( 2, 4));
+console.log( arr.slice( 0, 3))
+console.log( arr.slice(-2))
+console.log( arr.slice(-1))
+console.log( arr.slice(1,-2))
+console.log([...arr])// spread parametro to extrac data
+
+
+// SPLICE
+arr.splice(-1)
+console.log(arr)
+console.warn("////")
+arr.splice(1, 2)
+console.log(arr);
+
+// REVERSE PARAMETRO
+
+arr = ["a", "b", "c", "d", "e"]
+const arr2 = [ "f", "t", "i", "m", "j"]
+console.log(arr2.reverse());
+console.log(arr2);
+
+// CONCAT
+
+const letters = arr.concat(arr2)
+console.log(letters);
+console.log(...arr, ...arr2)
+
+//JOIN
+console.log(letters.join(" - "));
+
+
+const arr = [23, 11,64]
+console.log(arr[0])
+console.log(arr.at(0))
+// getting the last array element
+console.log( arr[arr.length -1]); // subtract the last number
+console.log(arr.slice(-1)[0])
+console.log(arr.at(-1));
+
+*/////
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+for( const movement of movements ){
+  if(movement > 0){
+    console.log( `You deposited ${movement}`);
+  } else {
+    console.log(`You withdrew ${Math.abs(movement)}`);
+  }
+}
+// we call the variable of the result 
+console.log("---------------FOR EACH METHOD-------------")
+movements.forEach(function(movement, index, array){// THIS part of the code we are using the callback to invocade
+if( movement = 0){// This is the 
+  console.log(`You deposited ${movement}`);
+}else {
+  console.log(`You withdre ${Math.abs(movement)}`);
+}
+});
+
+
+
